a07fb03ae146d7f3f2c8a863155dc57f194dfd06
Matthew Meisinger <mmeisinger@gmail.com>
2014-10-15 22:04:54 -0400

    Added second business rule, and made sure the error message appears in the UI.

--- api/BusinessService/CRMService.js
+++ api/BusinessService/CRMService.js
@@ -36,7 +36,6 @@ module.exports = {
 			// Agents in the USA, except those in TX and NY can have at most 5 customers.
 			// If attempting to assign this customer to an agent who has 5 other customers already, throw error.
 			if (item.agent) {
-				console.log("Agent found.");
 				var checkAgentCustomerLimit = AgentDS.get(item.agent)
 					.then(function (agent) {
 						console.log("Retrieved customer's agent, checking agent's customer limit.");
@@ -61,14 +60,49 @@ module.exports = {
 
 			// Business rule: 
 			// A customer can only update their contact information once/week, unless their agent is in MN or CT.
-			//var prevRecord = CustomerDS.get(id);
+			// Their 'Contact Information' consists of their first name, last name, email, and phone, and does not
+			// include their agent.
+			if (item.id) {
+				var checkUpdateContactInfoOnceAWeek = CustomerDS.get(item.id)
+					.then(function(customer) {
+						var contactChanged = false;
+						if (item.firstName != customer.firstName ||
+							item.lastName != customer.lastName ||
+							item.email != customer.email ||
+							item.phone != customer.phone) {
+							contactChanged = true;
+						}
+						console.log("Contact info changed? : " + contactChanged.toString());
+
+						if (contactChanged) {
+							// If the contact info changed, set updatedContactInfoAt field to the current date 
+							item.updatedContactInfoAt = new Date();
+
+							// If customer updated less than 7 days ago, throw an error
+							console.log("Checking if customer is updating their address more than once a week. Last updated their contact info at: " + customer.updatedContactInfoAt);
+							var sevenDaysAgo = new Date(new Date().setDate((new Date().getDate())-7));
+							console.log("Last updated: " + new Date(customer.updatedContactInfoAt));
+							console.log("Seven days ago: " + sevenDaysAgo);
+							if (new Date(customer.updatedContactInfoAt) > sevenDaysAgo) {
+								console.log("Customer updated less than 7 days ago. Save cancelled.");
+								throw "Customer cannot update their contact information more than once a week. Last updated: " + customer.updatedContactInfoAt;
+							}
+							else {
+								console.log("Customer updated more than 7 days ago.");
+							}
+						}
+					});
+				validationChecks.push(checkUpdateContactInfoOnceAWeek);
+			}
+
 
 			// Business rule: 
 			// Assigning a new agent to a customer is only allowed if there are no "contact records" less than 72 hours old.
 
+
 			// Make sure all checks pass, then if they do, save the customer record.
 			console.log('Checking all ' + validationChecks.length + ' validation checks relevant to this customer.');
-			return Q.all(validationChecks) // TODO: This doesn't seem to be waiting until all the validation checks pass before it continues on
+			return Q.all(validationChecks)
 				.then(function () {
 					console.log('All customer validation checks passed.');
 					return CustomerDS.save(item);
