f68473794641b7cdd5a41e741b8058db7007ec34
Johan Mena <johan.menac@gmail.com>
2014-11-17 22:28:34 -0500

    Add basic model for Subscription

---
+++ api/models/Subscription.js
@@ -0,0 +1,26 @@
+/**
+* Subscription.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    agent       : { model: 'agent' },
+    customerId  : { type: 'integer' },
+    parameter   : { type: 'string' },
+    value       : { type: 'string' },
+    operation   : {
+      type: 'string',
+      enum: ['create', 'update', 'destroy']
+    },
+    notification : {
+      type: 'string',
+      enum: ['email', 'push']
+    }
+  }
+
+};
+
