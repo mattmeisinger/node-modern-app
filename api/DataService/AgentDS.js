9a0744b0c451d8181f7db20cfcfa0d408416221f
Johan Mena <johan.menac@gmail.com>
2014-11-17 22:32:47 -0500

    Populate subscription fields on agent fetches

--- api/DataService/AgentDS.js
+++ api/DataService/AgentDS.js
@@ -1,20 +1,19 @@
-
 module.exports = {
-	getAll: function() {
-    return Agent.find().populate('customers');
-	},
-	get: function(id) {
-		return Agent.findOne(id).populate('customers');
-	},
-	save: function(item) {
-		if (item.id > 0) {
-			return Agent.update(item.id, item);
-		}
-		else {
-			return Agent.create(item);
-		}
-	},
-	delete: function(id) {
-		return Agent.destroy(id);
-	}
+  getAll: function() {
+    return Agent.find().populate('customers').populate('subscriptions');
+  },
+  get: function(id) {
+    return Agent.findOne(id).populate('customers').populate('subscriptions');
+  },
+  save: function(item) {
+    if (item.id > 0) {
+      return Agent.update(item.id, item);
+    }
+    else {
+      return Agent.create(item);
+    }
+  },
+  delete: function(id) {
+    return Agent.destroy(id);
+  }
 };
