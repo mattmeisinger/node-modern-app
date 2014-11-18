9463c34ad42c836567440abdeac123dea204e691
Johan Mena <johan.menac@gmail.com>
2014-11-17 22:29:03 -0500

    Create empty controller for subscriptions

---
+++ api/controllers/SubscriptionController.js
@@ -0,0 +1,11 @@
+/**
+ * SubscriptionController
+ *
+ * @description :: Server-side logic for managing subscriptions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
